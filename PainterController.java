d681c5653ae8897a790daaf888482431b60308df
Burulsun <45602889+Burulsun@users.noreply.github.com>
2020-12-23 07:51:24 +0600

    Add files via upload

---
+++ PainterController.java
@@ -0,0 +1,205 @@
+package Painter;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.event.MouseEvent;
+
+public class PainterController {
+
+
+    private enum PenSize{
+        SMALL(2),
+        MEDIUM(4),
+        LARGE(6);
+        private final int radius;
+
+        PenSize(int radius) {
+            this.radius = radius;
+        }
+        public int getRadius(){
+            return radius;
+        }
+    };
+
+
+    @FXML
+    private RadioButton blackRadioButton;
+
+    @FXML
+    private ToggleGroup colorToggleGroup;
+
+    @FXML
+    private RadioButton redRadioButton;
+
+    @FXML
+    private RadioButton greenRadioButton;
+
+    @FXML
+    private RadioButton blueRadioButton;
+
+    @FXML
+    private RadioButton smallRadioButton;
+
+    @FXML
+    private ToggleGroup sizeToggleGroup;
+
+    @FXML
+    private RadioButton mediumRadioButton;
+
+    @FXML
+    private RadioButton largeRadioButton;
+
+    @FXML
+    private Button undoButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private Slider redSlider;
+
+    @FXML
+    private Slider greenSlider;
+
+    @FXML
+    private Slider blueSlider;
+
+    @FXML
+    private Slider alphaSlider;
+
+    @FXML
+    private TextField redTextField;
+
+    @FXML
+    private TextField greenTextField;
+
+    @FXML
+    private TextField blueTextField;
+
+    @FXML
+    private TextField alphaTextField;
+
+    @FXML
+    private Circle colorCircle;
+
+    @FXML
+    private Rectangle colorRectangle;
+
+    private int red =0;
+    private int green =0;
+    private int blue =0;
+    private double alpha =1.0;
+
+
+    private PenSize radius =PenSize.MEDIUM;
+    private Paint brushColor = Color.BLACK;
+
+    public void initialize(){
+//        blackRadioButton.setUserData(Color.BLACK);
+//        redRadioButton.setUserData(Color.RED);
+//        greenRadioButton.setUserData(Color.GREEN);
+//        blueRadioButton.setUserData(Color.BLUE);
+        smallRadioButton.setUserData(PenSize.SMALL);
+        mediumRadioButton.setUserData(PenSize.MEDIUM);
+        largeRadioButton.setUserData(PenSize.LARGE);
+
+
+        redTextField.textProperty().bind(redSlider.valueProperty().asString("%.0f"));
+        greenTextField.textProperty().bind(greenSlider.valueProperty().asString("%.0f"));
+        blueTextField.textProperty().bind(blueSlider.valueProperty().asString("%.0f"));
+        alphaTextField.textProperty().bind(alphaSlider.valueProperty().asString("%.2f"));
+        brushColor = Color.rgb(red, green, blue, alpha);
+
+        redSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                       red = newValue.intValue();
+                       colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+
+                }
+                }
+        );
+
+        greenSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                        green = newValue.intValue();
+                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+//                        brushColor = Color.rgb(red, green, blue, alpha);
+                    }
+                }
+        );
+
+
+        blueSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                        blue = newValue.intValue();
+                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+//                        brushColor = Color.rgb(red, green, blue, alpha);
+                    }
+                }
+        );
+        alphaSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                        alpha = newValue.intValue();
+                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+//                        brushColor = Color.rgb(red, green, blue, alpha);
+                    }
+                }
+        );
+
+    }
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void colorRadioButtonSelected(ActionEvent event) {
+        //brushColor =(Color)colorToggleGroup.getSelectedToggle().getUserData();
+
+    }
+
+    @FXML
+    public void drawingAreaMouseDragged(javafx.scene.input.MouseEvent mouseEvent) {
+        Circle newCircle = new Circle(mouseEvent.getX(), mouseEvent.getY(),
+                radius.getRadius(), brushColor);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+
+
+    @FXML
+    void sizeRadioButtonSelected(ActionEvent event) {
+       radius =(PenSize)sizeToggleGroup.getSelectedToggle().getUserData();
+    }
+
+    @FXML
+    void undoButtonPressed(ActionEvent event) {
+        int count = drawingAreaPane.getChildren().size();
+        if(count>0){
+            drawingAreaPane.getChildren().remove(count-1);
+        }
+
+    }
+
+    
+}
---
+++ PainterMain.java
@@ -0,0 +1,26 @@
+package Painter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class PainterMain extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Painter.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
